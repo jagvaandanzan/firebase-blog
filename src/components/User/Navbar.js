72c355d37bfcdec16a23d8cc92d93ee157afac53
jagvaandanzan <aagi.j@yahoo.com>
2022-06-18 21:43:35 +0800

    navbar button fix

--- src/components/User/Navbar.js
+++ src/components/User/Navbar.js
@@ -17,14 +17,14 @@ export default function Navbar() {
   };
 
   return (
-    <div className="flex w-full h-10 px-5 md:h-16 md:px-16  md:mb-5  justify-between items-center shadow">
-      <div className="flex w-full justify-between">
+    <div className="w-full h-10 px-5 md:h-16 md:px-16  md:mb-5   shadow">
+      <div className="flex w-full justify-between md:hidden">
         <button>Home</button>
         <button>
           <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAKElEQVRIiWNgGAXDHjAisf/TwmwmKhs6CgYhGE1Fo4ByMJqKRsEQAADWCQMKYvEFtQAAAABJRU5ErkJggg==" />
         </button>
       </div>
-      <div className="hidden">
+      <div className="hidden md:flex md:justify-between md:h-full">
         <div className="h-full flex items-center ">
           <button className="h-full w-24 border-b-4 border-transparent focus:border-blue-500 hover:text-blue-900" onClick={() => navigate("/")}>
             Home
